05cdc78833e8e208c3b643b7ea6f7793cfee39ac
Boitumelo Monyebodi <linkymonyebodi1@gmail.com>
2025-04-13 15:22:25 +0200

    Create Publisher.sql

---
+++ Publisher.sql
@@ -0,0 +1,8 @@
+Publisher
+INSERT INTO publisher (name, country_id) VALUES 
+('MacMillan', 001),
+('Little,Brown and Company', 002),
+('Secker&Warburg', 003),
+('Edward Blount,William Shakespeare and Isaac Jaggard', 004),
+('Harper Collins', 005);
+
